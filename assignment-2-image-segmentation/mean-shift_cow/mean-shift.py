a2af2fa31744b829c9d86cbc9af525570ca17190
GrapixLeGrand <misterbibiche@gmail.com>
2021-11-05 14:25:16 +0100

    added things

--- assignment-2-image-segmentation/mean-shift_cow/mean-shift.py
+++ assignment-2-image-segmentation/mean-shift_cow/mean-shift.py
@@ -17,18 +17,17 @@ def distance(x, X):
     return torch.sum((X - x) ** 2, 1) ** 0.5
 
 def distance_batch(x, X):
-    raise NotImplementedError('distance_batch function not implemented!')
+    #raise NotImplementedError('distance_batch function not implemented!')
+    
+    return torch.norm(x - X, dim=0) #torch.sum((X - x) ** 2, 1) ** 0.5
 
 #https://en.wikipedia.org/wiki/Radial_basis_function_kernel
 #looks like the bandwith is the sigma in the rbf after this post
 #https://moodle-app2.let.ethz.ch/mod/forum/discuss.php?d=88657
 def gaussian(dist, bandwidth):
-    #raise NotImplementedError('gaussian function not implemented!')
     return torch.exp(-(dist ** 2) / (2 * bandwidth ** 2))
 
 def update_point(weight, X):
-    #raise NotImplementedError('update_point function not implemented!')
-
     # unsqueeze weight (increase the whole dimension by 1: [] -> [[]])
     # then expand this dimension by 3 with same elements
     weight_ = torch.unsqueeze(weight, 0).expand(3, weight.size()[0])
@@ -39,11 +38,24 @@ def update_point(weight, X):
     weight_sum = torch.sum(weight)
 
     result = X_weighted_sum / weight_sum
-    assert result.size() == X[0].size() # in case a ghost flattening happens
+    #assert result.size() == X[0].size() # in case a ghost flattening happens
     return result
 
 def update_point_batch(weight, X):
-    raise NotImplementedError('update_point_batch function not implemented!')
+    print("update points X : ", X.size(), ", weight : ", weight.size())
+    #raise NotImplementedError('update_point_batch function not implemented!')
+    # unsqueeze weight (increase the whole dimension by 1: [] -> [[]])
+    # then expand this dimension by 3 with same elements
+    weight_ = torch.unsqueeze(weight, 0).expand(3, weight.size()[0])
+    weight_ = torch.transpose(weight_, 0, 1) #swap axis 0 and 1
+
+    #compute the shift as described here: https://en.wikipedia.org/wiki/Mean_shift
+    X_weighted_sum = torch.sum(weight_ * X, axis=0)
+    weight_sum = torch.sum(weight)
+
+    result = X_weighted_sum / weight_sum
+    #assert result.size() == X[0].size() # in case a ghost flattening happens
+    return result
 
 def meanshift_step(X, bandwidth=2.5):
     X_ = X.clone()
@@ -54,14 +66,24 @@ def meanshift_step(X, bandwidth=2.5):
     return X_
 
 def meanshift_step_batch(X, bandwidth=2.5):
-    raise NotImplementedError('meanshift_step_batch function not implemented!')
+    X = X.clone()
+
+    dists = distance_batch(X, X)
+    print("dist : ", dists.size())
+    print(dists)
+    weights = gaussian(dists, bandwidth)
+    X_ = update_point_batch(weights, X)
+
+    return X_
+
+    #raise NotImplementedError('meanshift_step_batch function not implemented!')
 
 def meanshift(X):
     X = X.clone()
     for i in range(20):
         print("iteration: ", i)
-        X = meanshift_step(X)   # slow implementation
-        # X = meanshift_step_batch(X)   # fast implementation
+        #X = meanshift_step(X)   # slow implementation
+        X = meanshift_step_batch(X)   # fast implementation
     return X
 
 scale = 0.25    # downscale the image to run faster
